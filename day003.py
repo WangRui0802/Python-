291a9b4c8baf8c649c8871a0533fd4a0cdfeb42c
WangRui0802 <wangrui0802@126.com>
2019-07-24 21:07:53 +0800

    day003

---
+++ day003.py
@@ -0,0 +1,13 @@
+# 编写一个程序，检查3个变量x、y和z，输出其中最大的奇数。如果其中没有奇数，就输出一个消息进行说明。
+# 思路：先找到3个变量里最大的数，再判断那个数的奇偶性
+if x < y and x < z;
+    print('x is least')
+    elif y < z;
+        print('y is least')
+    else:
+        print('z is least')
+elif a%2 !=0;
+print('a')
+else:
+    print('x,y,z中没有最大的奇数')
+    
